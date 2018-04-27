8205527a1228ffa048f5d3c1c0d4f9939f948e1d
Govindraj Dhage <graj3470@gmail.com>
2018-04-27 19:30:38 +0530

    UI changes

--- webApp/src/app/components/dashboard/info-overlay/info-overlay.component.ts
+++ webApp/src/app/components/dashboard/info-overlay/info-overlay.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-info-overlay',
   templateUrl: './info-overlay.component.html',
-  styleUrls: ['./info-overlay.component.scss']
+  styleUrls: ['./info-overlay.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class InfoOverlayComponent implements OnInit, OnDestroy {
   @Input() display;
+  @Input() listItem;
   @Output() showEvent = new EventEmitter();
 
   constructor() { }
