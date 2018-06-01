d96383ea32a48705dc8b2d41d45a300969ede724
Govindraj Dhage <graj3470@gmail.com>
2018-06-01 22:48:31 +0530

    UI changes

--- webApp/src/app/components/dashboard/dashboard.module.ts
+++ webApp/src/app/components/dashboard/dashboard.module.ts
@@ -20,7 +20,7 @@ import { InfoOverlayComponent } from './info-overlay/info-overlay.component';
 
 
 @NgModule({
-  imports: [BrowserAnimationsModule, HttpModule, CommonModule, FormsModule, RouterModule, ReactiveFormsModule, AccordionModule, InfiniteScrollModule, DialogModule],
+  imports: [BrowserAnimationsModule, HttpModule, CommonModule, FormsModule, RouterModule, ReactiveFormsModule, AccordionModule, InfiniteScrollModule, DialogModule, GrowlModule],
   declarations: [DashboardComponent, HashBlockComponent, TranscationBlockComponent, InfoOverlayComponent],
   exports: [DashboardComponent],
   //providers: [CommonServiceService, MessageService],
